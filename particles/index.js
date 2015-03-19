04c70939576b2b5171411d231cc7a01438fd9fa5
joakin <joaquin@chimeces.com>
2015-03-19 02:35:59 +0100

    Particles demo

--- particles/index.js
+++ particles/index.js
@@ -1,5 +1,616 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+require('canvas-testbed')(render, start)
+var h = require('../../canvas-helpers')
 
-console.log('hi')
+var mouse
+var particles
+var numParticles = 200
+var minParticleSize = 2
+var maxParticleSize = 5
+function start(context, width, height) {
+  this.canvas.addEventListener('click', onClick);
+  function onClick() {
+    numParticles = 1 + Math.random()*400
+    minParticleSize = 2 + Math.random()*10
+    maxParticleSize = minParticleSize + Math.random()*20
+    init(context, width, height)
+  }
+
+  this.canvas.addEventListener('mousemove', onMousemove);
+  function onMousemove(e) {
+    mouse.x = e.clientX;
+    mouse.y = e.clientY;
+  }
+
+  init(context, width, height)
+}
+
+function init(context, width, height) {
+  mouse = { x: width * 0.5, y: height * 0.5 }
+
+  particles = []
+  var dim = Math.min(width, height)
+  for ( var i = 0; i < numParticles; i++ ) {
+    particles[i] = new Particle(
+      mouse.x, mouse.y,
+      (Math.random()*dim - dim/2)/14,
+      (Math.random()*dim - dim/2)/14,
+      'hsla('+(i*100/numParticles)+', 80%, 40%, 1)'
+    )
+  }
+}
+
+var msg = 'Click to randomly regenerate particles'
+function render(context, width, height) {
+  h.clear(0.2, context, width, height)
+  h.drawFPS(this.fps, context)
+  h.drawBottomCenteredText(msg, context, width, height)
+  for ( var i = 0; i < particles.length; i++ ) {
+    particles[i].bounce(0, 0, width, height)
+    particles[i].attract(mouse)
+    particles[i].update()
+    particles[i].draw(context)
+  }
+}
+
+function Particle(x, y, vx, vy, color) {
+  this.x = this.oldx = x
+  this.y = this.oldy = y
+  this.vx = vx
+  this.vy = vy
+  this.color = color
+  this.friction = 0.999
+  this.size = minParticleSize+Math.random()*maxParticleSize
+}
+
+Particle.prototype.update = function() {
+  this.vx *= this.friction
+  this.vy *= this.friction
+  this.oldx = this.x
+  this.oldy = this.y
+  this.x += this.vx
+  this.y += this.vy
+}
+
+Particle.prototype.draw = function(context) {
+  context.strokeStyle = this.color
+  context.fillStyle = this.color
+  h.line(context, this.oldx, this.oldy, this.x, this.y, this.size)
+  h.fillCircle(context, this.x, this.y, this.size/2)
+}
+
+Particle.prototype.attract = function(to) {
+  var dx = to.x - this.x
+  var dy = to.y - this.y
+  var distance = Math.sqrt(dx * dx + dy * dy)
+  if (distance === 0) { distance = 1 }
+  this.vx += dx / distance
+  this.vy += dy / distance
+}
+
+Particle.prototype.bounce = function(x, y, w, h) {
+  var slow = 0.3
+  if (this.x <= x) this.vx = Math.abs(this.vx) * slow
+  if (this.x >= w) this.vx = - Math.abs(this.vx) * slow
+  if (this.y <= y) this.vy = Math.abs(this.vy) * slow
+  if (this.y >= h) this.vy = - Math.abs(this.vy) * slow
+}
+
+
+},{"../../canvas-helpers":2,"canvas-testbed":3}],2:[function(require,module,exports){
+exports.clear = function(alpha, context, width, height) {
+  context.fillStyle = 'rgba(0,0,0,' + alpha || 0.5 + ')'
+  context.fillRect(0, 0, width, height)
+}
+
+exports.drawFPS = function(fps, context) {
+  context.font = 'bold 12pt Courier'
+  context.textAlign = 'left'
+  context.fillStyle = 'rgba(255,0,0,1)'
+  context.fillText(fps + 'fps', 10, 20)
+}
+
+exports.drawBottomCenteredText = function(text, context, width, height) {
+  context.font = 'bold 12pt sans-serif'
+  context.textAlign = 'center'
+  context.fillStyle = 'rgba(255,255,0,1)'
+  context.fillText(text, width / 2, height - 20)
+}
+
+exports.fillCircle = function(context, x, y, r) {
+  context.beginPath()
+  context.arc(x, y, r, 0, 2 * Math.PI)
+  context.fill()
+}
+
+exports.line = function(context, px, py, qx, qy, w) {
+  context.lineWidth = w
+  context.beginPath()
+  context.moveTo(px, py)
+  context.lineTo(qx, qy)
+  context.stroke()
+}
+
+},{}],3:[function(require,module,exports){
+var domready = require('domready');
+require('raf.js');
+
+var CanvasApp = require('canvas-app');
+
+module.exports = function( render, start, options ) {
+	domready(function() {
+		//options were provided as the first argument, no render handler
+		if (typeof render === "object" && render) {
+			options = render;
+			render = null;
+			start = null;
+		}
+		//options were provided as the second argument
+		else if (typeof start === "object" && start) {
+			options = start;
+			start = null;
+		}
+		//otherwise options were provided as the third argument...
+
+		
+		options = options||{};
+		if (typeof options.onReady !== 'function')
+			options.onReady = start;
+
+		var runner = CanvasApp(render, options);
+
+		runner.canvas.setAttribute("id", "canvas");
+		document.body.appendChild(runner.canvas);
+		document.body.style.margin = "0";
+		document.body.style.overflow = "hidden";
+		
+		runner.start();
+	});
+};
+},{"canvas-app":4,"domready":10,"raf.js":11}],4:[function(require,module,exports){
+var isGL = require('is-webgl-context');
+var getGL = require('webgl-context');
+var debounce = require('debounce');
+var addEvent = require('add-event-listener');
+
+function isCanvasContext(obj) {
+    var ctx2d = typeof CanvasRenderingContext2D !== 'undefined' && obj instanceof CanvasRenderingContext2D;
+    return obj && (ctx2d || isGL(obj));
+}
+
+function CanvasApp(render, options) {
+    if (!(this instanceof CanvasApp))
+        return new CanvasApp(render, options);
+
+    //allow options to be passed as first argument
+    if (typeof render === 'object' && render) {
+        options = render;
+        render = null;
+    }
+
+    render = typeof render === 'function' ? render : options.onRender;
+
+    options = options||{};
+    options.retina = typeof options.retina === "boolean" ? options.retina : true;
+    
+    var hasWidth = typeof options.width === "number", 
+        hasHeight = typeof options.height === "number";
+
+    //if either width or height is specified, don't auto-resize to the window...
+    if (hasWidth || hasHeight) 
+        options.ignoreResize = true;
+
+    options.width = hasWidth ? options.width : window.innerWidth;
+    options.height = hasHeight ? options.height : window.innerHeight;
+
+    var DPR = options.retina ? (window.devicePixelRatio||1) : 1; 
+
+    //setup the canvas
+    var canvas,
+        context,
+        attribs = options.contextAttributes||{};
+
+    this.isWebGL = false;
+
+    //if user provided a context object
+    if (isCanvasContext(options.context)) {
+        context = options.context;
+        canvas = context.canvas;
+    }
+
+    //otherwise allow for a string to set one up
+    if (!canvas)
+        canvas = options.canvas || document.createElement("canvas");
+
+    canvas.width = options.width * DPR;
+    canvas.height = options.height * DPR;
+
+    if (!context) {
+        if (options.context === "webgl" || options.context === "experimental-webgl") {
+            context = getGL({ canvas: canvas, attributes: attribs });
+            if (!context) {
+                throw "WebGL Context Not Supported -- try enabling it or using a different browser";
+            }
+        } else {
+            context = canvas.getContext(options.context||"2d", attribs);
+        }
+    }
+
+    this.isWebGL = isGL(context);
+
+    if (options.retina) {
+        canvas.style.width = options.width + 'px';
+        canvas.style.height = options.height + 'px';
+    }
+
+    this.running = false;
+    this.width = options.width;
+    this.height = options.height;
+    this.canvas = canvas;
+    this.context = context;
+    this.onResize = options.onResize;
+    this._DPR = DPR;
+    this._retina = options.retina;
+    this._once = options.once;
+    this._ignoreResize = options.ignoreResize;
+    this._lastFrame = null;
+    this._then = Date.now();
+    this.maxDeltaTime = typeof options.maxDeltaTime === 'number' ? options.maxDeltaTime : 1000/24;
+
+    //FPS counter
+    this.fps = 60;
+    this._frames = 0;
+    this._prevTime = this._then;
+
+    if (!this._ignoreResize) {
+        options.resizeDebounce = typeof options.resizeDebounce === 'number'
+                    ? options.resizeDebounce : 50;
+        addEvent(window, "resize", debounce(function() {
+            this.resize(window.innerWidth, window.innerHeight);
+        }.bind(this), options.resizeDebounce, false));
+
+        addEvent(window, "orientationchange", function() {
+            this.resize(window.innerWidth, window.innerHeight);
+        }.bind(this));
+    }
+
+    if (typeof render === "function") {
+        this.onRender = render.bind(this);   
+    } else {
+        //dummy render function
+        this.onRender = function (context, width, height, dt) { };
+    }
+
+    this.renderOnce = function() {
+        var now = Date.now();
+        var dt = Math.min(this.maxDeltaTime, (now-this._then));
+
+        this._frames++;
+        if (now > this._prevTime + 1000) {
+            this.fps = Math.round((this._frames * 1000) / (now - this._prevTime));
+
+            this._prevTime = now;
+            this._frames = 0;
+        }
+
+        if (!this.isWebGL) {
+            this.context.save();
+            this.context.scale(this._DPR, this._DPR);
+        } else {
+            this.context.viewport(0, 0, this.width * this._DPR, this.height * this._DPR);
+        }
+        
+        this.onRender(this.context, this.width, this.height, dt);
+
+        if (!this.isWebGL)
+            this.context.restore();
+
+        this._then = now;
+    };
+
+    this._renderHandler = function() {
+        if (!this.running) 
+            return;
+        
+        if (!this._once) {
+            this._lastFrame = requestAnimationFrame(this._renderHandler);
+        }
+
+        this.renderOnce();
+    }.bind(this);
+
+    if (typeof options.onReady === "function") {
+        options.onReady.call(this, context, this.width, this.height);
+    }
+}
+
+Object.defineProperty(CanvasApp.prototype, 'retinaEnabled', {
+
+    set: function(v) {
+        this._retina = v;
+        this._DPR = this._retina ? (window.devicePixelRatio||1) : 1;
+        this.resize(this.width, this.height);
+    },
+
+    get: function() {
+        return this._retina;
+    }
+});
+
+Object.defineProperty(CanvasApp.prototype, 'deviceWidth', {
+
+    get: function() {
+        return this.width * this._DPR;
+    }
+});
+
+Object.defineProperty(CanvasApp.prototype, 'deviceHeight', {
+
+    get: function() {
+        return this.height * this._DPR;
+    }
+});
+
+CanvasApp.prototype.resetFPS = function() {
+    this._frames = 0;
+    this._prevTime = Date.now();
+    this._then = this._prevTime;
+    this.fps = 60;
+};
+
+CanvasApp.prototype.start = function() {
+    if (this.running)
+        return;
+    
+    if (this._lastFrame) 
+        cancelAnimationFrame(this._lastFrame);
+
+    //reset FPS counter
+    this.resetFPS();
+
+    this.running = true;
+    this._lastFrame = requestAnimationFrame(this._renderHandler);
+};
+
+CanvasApp.prototype.stop = function() {
+    if (this._lastFrame) {
+        cancelAnimationFrame(this._lastFrame);
+        this._lastFrame = null;
+    }
+    this.running = false;
+};
+
+CanvasApp.prototype.resize = function(width, height) {
+    var canvas = this.canvas;
+
+    this.width = width;
+    this.height = height;
+    canvas.width = this.width * this._DPR;
+    canvas.height = this.height * this._DPR;
+
+    if (this._retina) {
+        canvas.style.width = this.width + 'px';
+        canvas.style.height = this.height + 'px';
+    }
+
+    if (this._once)
+        requestAnimationFrame(this._renderHandler);
+    if (typeof this.onResize === "function")
+        this.onResize(this.width, this.height);
+};
+
+module.exports = CanvasApp;
+},{"add-event-listener":5,"debounce":6,"is-webgl-context":8,"webgl-context":9}],5:[function(require,module,exports){
+addEventListener.removeEventListener = removeEventListener
+addEventListener.addEventListener = addEventListener
+
+module.exports = addEventListener
+
+var Events = null
+
+function addEventListener(el, eventName, listener, useCapture) {
+  Events = Events || (
+    document.addEventListener ?
+    {add: stdAttach, rm: stdDetach} :
+    {add: oldIEAttach, rm: oldIEDetach}
+  )
+  
+  return Events.add(el, eventName, listener, useCapture)
+}
+
+function removeEventListener(el, eventName, listener, useCapture) {
+  Events = Events || (
+    document.addEventListener ?
+    {add: stdAttach, rm: stdDetach} :
+    {add: oldIEAttach, rm: oldIEDetach}
+  )
+  
+  return Events.rm(el, eventName, listener, useCapture)
+}
+
+function stdAttach(el, eventName, listener, useCapture) {
+  el.addEventListener(eventName, listener, useCapture)
+}
+
+function stdDetach(el, eventName, listener, useCapture) {
+  el.removeEventListener(eventName, listener, useCapture)
+}
+
+function oldIEAttach(el, eventName, listener, useCapture) {
+  if(useCapture) {
+    throw new Error('cannot useCapture in oldIE')
+  }
+
+  el.attachEvent('on' + eventName, listener)
+}
+
+function oldIEDetach(el, eventName, listener, useCapture) {
+  el.detachEvent('on' + eventName, listener)
+}
+
+},{}],6:[function(require,module,exports){
+
+/**
+ * Module dependencies.
+ */
+
+var now = require('date-now');
+
+/**
+ * Returns a function, that, as long as it continues to be invoked, will not
+ * be triggered. The function will be called after it stops being called for
+ * N milliseconds. If `immediate` is passed, trigger the function on the
+ * leading edge, instead of the trailing.
+ *
+ * @source underscore.js
+ * @see http://unscriptable.com/2009/03/20/debouncing-javascript-methods/
+ * @param {Function} function to wrap
+ * @param {Number} timeout in ms (`100`)
+ * @param {Boolean} whether to execute at the beginning (`false`)
+ * @api public
+ */
+
+module.exports = function debounce(func, wait, immediate){
+  var timeout, args, context, timestamp, result;
+  if (null == wait) wait = 100;
+
+  function later() {
+    var last = now() - timestamp;
+
+    if (last < wait && last > 0) {
+      timeout = setTimeout(later, wait - last);
+    } else {
+      timeout = null;
+      if (!immediate) {
+        result = func.apply(context, args);
+        if (!timeout) context = args = null;
+      }
+    }
+  };
+
+  return function debounced() {
+    context = this;
+    args = arguments;
+    timestamp = now();
+    var callNow = immediate && !timeout;
+    if (!timeout) timeout = setTimeout(later, wait);
+    if (callNow) {
+      result = func.apply(context, args);
+      context = args = null;
+    }
+
+    return result;
+  };
+};
+
+},{"date-now":7}],7:[function(require,module,exports){
+module.exports = Date.now || now
+
+function now() {
+    return new Date().getTime()
+}
+
+},{}],8:[function(require,module,exports){
+module.exports = function(ctx) {
+	if (!ctx) return false
+	var gl = ctx
+	//compatibility with Chrome WebGL Inspector Addon
+	if (typeof ctx.rawgl !== 'undefined')
+		gl = ctx.rawgl
+	if (typeof WebGLRenderingContext !== 'undefined' && gl instanceof WebGLRenderingContext)
+		return true
+	return false
+}
+},{}],9:[function(require,module,exports){
+module.exports = function(opts) {
+    opts = opts||{};
+    var canvas = opts.canvas || document.createElement("canvas");
+    if (typeof opts.width === "number")
+        canvas.width = opts.width;
+    if (typeof opts.height === "number")
+        canvas.height = opts.height;
+    
+    var attribs = (opts.attributes || opts.attribs || {});
+    try {
+        gl = (canvas.getContext('webgl', attribs) || canvas.getContext('experimental-webgl', attribs));
+    } catch (e) {
+        gl = null;
+    }
+    return gl;
+};
+},{}],10:[function(require,module,exports){
+/*!
+  * domready (c) Dustin Diaz 2014 - License MIT
+  */
+!function (name, definition) {
+
+  if (typeof module != 'undefined') module.exports = definition()
+  else if (typeof define == 'function' && typeof define.amd == 'object') define(definition)
+  else this[name] = definition()
+
+}('domready', function () {
+
+  var fns = [], listener
+    , doc = document
+    , hack = doc.documentElement.doScroll
+    , domContentLoaded = 'DOMContentLoaded'
+    , loaded = (hack ? /^loaded|^c/ : /^loaded|^i|^c/).test(doc.readyState)
+
+
+  if (!loaded)
+  doc.addEventListener(domContentLoaded, listener = function () {
+    doc.removeEventListener(domContentLoaded, listener)
+    loaded = 1
+    while (listener = fns.shift()) listener()
+  })
+
+  return function (fn) {
+    loaded ? fn() : fns.push(fn)
+  }
+
+});
+
+},{}],11:[function(require,module,exports){
+/*
+ * raf.js
+ * https://github.com/ngryman/raf.js
+ *
+ * original requestAnimationFrame polyfill by Erik Möller
+ * inspired from paul_irish gist and post
+ *
+ * Copyright (c) 2013 ngryman
+ * Licensed under the MIT license.
+ */
+
+(function(window) {
+	var lastTime = 0,
+		vendors = ['webkit', 'moz'],
+		requestAnimationFrame = window.requestAnimationFrame,
+		cancelAnimationFrame = window.cancelAnimationFrame,
+		i = vendors.length;
+
+	// try to un-prefix existing raf
+	while (--i >= 0 && !requestAnimationFrame) {
+		requestAnimationFrame = window[vendors[i] + 'RequestAnimationFrame'];
+		cancelAnimationFrame = window[vendors[i] + 'CancelAnimationFrame'];
+	}
+
+	// polyfill with setTimeout fallback
+	// heavily inspired from @darius gist mod: https://gist.github.com/paulirish/1579671#comment-837945
+	if (!requestAnimationFrame || !cancelAnimationFrame) {
+		requestAnimationFrame = function(callback) {
+			var now = +new Date(), nextTime = Math.max(lastTime + 16, now);
+			return setTimeout(function() {
+				callback(lastTime = nextTime);
+			}, nextTime - now);
+		};
+
+		cancelAnimationFrame = clearTimeout;
+	}
+
+	// export to window
+	window.requestAnimationFrame = requestAnimationFrame;
+	window.cancelAnimationFrame = cancelAnimationFrame;
+}(window));
 
 },{}]},{},[1]);
